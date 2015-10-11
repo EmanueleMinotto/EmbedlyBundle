3885c92e83fd207ca01516ed764e089b0afd618c
Travis <travis@travis-ci.org>
2015-10-11 09:50:55 +0000

    API updated

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","EmanueleMinotto\\EmbedlyBundle\\DependencyInjection\\Configuration"],["c","EmanueleMinotto\\EmbedlyBundle\\DependencyInjection\\EmbedlyExtension"],["c","EmanueleMinotto\\EmbedlyBundle\\EmbedlyBundle"]];
